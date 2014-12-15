5d9eb9884c2848d4f60d93b4400c6a6596a15c80
PTC <pheartheceal@gmail.com>
2014-12-15 14:30:39 -0800

    version 0.9

--- volapi/volapi.py
+++ volapi/volapi.py
@@ -32,7 +32,7 @@ from threading import Barrier, Condition, Thread
 
 from .multipart import Data
 
-__version__ = "0.8.9"
+__version__ = "0.9"
 
 BASE_URL = "https://volafile.io"
 BASE_ROOM_URL = BASE_URL + "/r/"
