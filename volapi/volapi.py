cf3123164009600887be2da687fc8203bd677a83
PTC <pheartheceal@gmail.com>
2015-01-23 15:43:25 -0800

    18.1

--- volapi/volapi.py
+++ volapi/volapi.py
@@ -47,7 +47,7 @@ from autobahn.asyncio.websocket import WebSocketClientProtocol
 
 from .multipart import Data
 
-__version__ = "0.18"
+__version__ = "0.18.1"
 
 BASE_URL = "https://volafile.io"
 BASE_ROOM_URL = BASE_URL + "/r/"
