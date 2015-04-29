c74faa2ac82cace52f09e2e52a495a198aa4744b
RealDolos <dolosthegreat@safe-mail.net>
2015-04-29 14:41:15 +0200

    Actually convert server expire time to pythonic time

--- volapi/volapi.py
+++ volapi/volapi.py
@@ -366,7 +366,7 @@ class Room:
             file = File(self.conn, file[0], file[1],
                         type=file[2],
                         size=file[3],
-                        expire_time=file[4],
+                        expire_time=int(file[4]) / 1000,
                         uploader=file[6]['user'])
             self._files[file.id] = file
             self.conn.enqueue_data("file", file)
