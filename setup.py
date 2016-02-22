1106f53433f472a97e43ec5dbf4df6337f834194
RealDolos <dolosthegreat@safe-mail.net>
2016-02-22 12:36:57 +0100

    3.5 tag

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         "Operating System :: OS Independent",
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
