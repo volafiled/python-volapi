926da47e523fb2718cc4f865f65293a3fd7390e9
RealDolos <dolosthegreat@safe-mail.net>
2016-02-22 12:36:23 +0100

    Relicense stuff under MIT

--- volapi/__init__.py
+++ volapi/__init__.py
@@ -1,18 +1,7 @@
-'''
-This file is part of Volapi.
-
-Volapi is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Volapi is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Volapi.  If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
+The MIT License (MIT)
+Copyright © 2015 RealDolos
+See LICENSE
+"""
 
 from .volapi import Room, listen_many
